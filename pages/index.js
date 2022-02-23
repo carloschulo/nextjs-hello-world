517d9fcb15a357a66bc1ac780a4e6b93e53aca88
Carlos Henriquez <carloschulo@gmail.com>
2022-02-23 14:31:16 -0500

    add api to test on render

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
-import Link from 'next/link'
-const Index = () => (
-  <div>
-    Hello World.{' '}
-    <Link href="/about">
-      <a>About</a>
-    </Link>
-  </div>
-)
+import Link from "next/link";
+import { useEffect } from "react";
+const Index = () => {
+  useEffect(() => {
+    console.log("index");
+    fetch("/api/test")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+  return (
+    <div>
+      Hello World.{" "}
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </div>
+  );
+};
 export default Index;
